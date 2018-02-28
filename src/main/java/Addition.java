7f812f0cb5558ac7b78a150105a9c097e00a54a6
Archit Chauhan <archit.chauha@tothenew.com>
2018-02-28 16:58:50 +0530

    Archit

---
+++ src/main/java/Addition.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Addition {
+
+    int sum(int a, int b) {
+        return a + b;
+    }
+
+    String sum(String fname, String lname) {
+        return fname + lname;
+    }
+
+    List sum(List lista, List listb) {
+        List list = new ArrayList();
+        list.addAll(lista);
+        list.addAll(listb);
+        return list;
+    }
+
+    void methodExc() throws ArithmeticException {
+       throw  new ArithmeticException();
+    }
+
+    Float sum(Float a, Float b){
+        return a + b;
+    }
+}
---
+++ src/main/java/Subtraction.java
@@ -0,0 +1,11 @@
+public class Subtraction {
+    int diff(int a, int b) {
+        return a - b;
+    }
+    double diff(double a, double b) {
+        return a - b;
+    }
+    float diff(float a, float b) {
+        return a - b;
+    }
+}
